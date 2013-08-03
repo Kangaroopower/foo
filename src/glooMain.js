664fb86661e65416dfa1cfa299c6c0bb14539165
Kangaroopower <racecar360@gmail.com>
2013-08-03 12:56:33 -0700

    fix bugs related to a changed variable name

--- src/glooMain.js
+++ src/glooMain.js
@@ -1094,9 +1094,9 @@ iglooHist.prototype.getHistory = function (callback, data) {
  
 			var pageHistory = '';
 			for (var i in data) {
-				var rev = data[i],  = revision.user;
+				var rev = data[i];
  
-				pageHistory += '<li id="iglooHist_'+revision.ids.revid+'" onclick="igloo.actions.loadPage(\''+me.pageTitle.replace('\'', '\\\'')+'\',  \''+rev.ids.revid+'\');" onmouseover="this.style.backgroundColor = \''+jin.Colour.LIGHT_GREY+'\';" onmouseout="this.style.backgroundColor = \''+jin.Colour.WHITE+'\';" style="cursor: pointer; width: 186px; padding: 2px; border-bottom: 1px solid #000000; list-style-type: none; list-style-image: none; marker-offset: 0px; background-color: '+jin.Colour.WHITE+';">'+revision.user+'</li>';
+				pageHistory += '<li id="iglooHist_'+rev.ids.revid+'" onclick="igloo.actions.loadPage(\''+me.pageTitle.replace('\'', '\\\'')+'\',  \''+rev.ids.revid+'\');" onmouseover="this.style.backgroundColor = \''+jin.Colour.LIGHT_GREY+'\';" onmouseout="this.style.backgroundColor = \''+jin.Colour.WHITE+'\';" style="cursor: pointer; width: 186px; padding: 2px; border-bottom: 1px solid #000000; list-style-type: none; list-style-image: none; marker-offset: 0px; background-color: '+jin.Colour.WHITE+';">'+rev.user+'</li>';
 			}
 			
 			pageHistory += '<li style="width: 100%; list-style-type: none; list-style-image: none; text-align: center;"><a target="_blank" href="'+ wgServer + wgScript + '?title=' + me.pageTitle + '&action=history">- full history -</a></li>';

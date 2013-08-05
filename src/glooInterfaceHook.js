293bab6202537b0255f662962131265cc9617269
Kangaroopower <racecar360@gmail.com>
2013-08-05 10:13:59 -0700

    remove a useless if statement

--- src/glooInterfaceHook.js
+++ src/glooInterfaceHook.js
@@ -17,15 +17,12 @@
 				var iglooDivs = document.getElementsByTagName('div');
 				
 				// check for launch buttons
-				if (iglooSettings.hookInterface == true) {
-					// hook the mediawiki interface
-					var iglooLink = document.createElement('li');
-					iglooLink.id = 't-igloo';
-					iglooLink.innerHTML = '<a id="igloo-goto-menu" target="_blank" href="User:Ale_jrb/Scripts/igloo" title="igloo">igloo</a> | <a id="igloo-do-launch" target="_blank" href="User:Ale_jrb/igDev" title="launch igloo">(launch)</a>';
+				var iglooLink = document.createElement('li');
+				iglooLink.id = 't-igloo';
+				iglooLink.innerHTML = '<a id="igloo-goto-menu" target="_blank" href="User:Ale_jrb/Scripts/igloo" title="igloo">igloo</a> | <a id="igloo-do-launch" target="_blank" href="User:Ale_jrb/igDev" title="launch igloo">(launch)</a>';
 					 
-					var parent = document.getElementById('p-tb');
-					parent.childNodes[3].childNodes[1].insertBefore(iglooLink, parent.childNodes[3].childNodes[1].firstChild);
-				}
+				var parent = document.getElementById('p-tb');
+				parent.childNodes[3].childNodes[1].insertBefore(iglooLink, parent.childNodes[3].childNodes[1].firstChild);
 				
 				for ( var i = 0; i < iglooDivs.length; i++ ) {
 					if ( iglooDivs[i].className == 'iglooNotInstalled' ) {
